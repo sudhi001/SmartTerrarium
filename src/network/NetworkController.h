020500ce59b471f7b3faa5e41e03fae555f750b4
Sudhi S <sudhis@live.com>
2024-04-21 16:45:50 +0530

    password reset

--- src/network/NetworkController.h
+++ src/network/NetworkController.h
@@ -19,7 +19,7 @@ private:
   NetworCridentials networkSCridentials;
   bool isStorageEmpty();
   const char *ssid = "SmartAgro";
-  const char *password = "Sm123";
+  const char *password = "0123456789";
   const int channel = 10;       // WiFi Channel number between 1 and 13
   const bool hide_SSID = false; // To disable SSID broadcast -> SSID will not appear in a basic WiFi scan
   const int max_connection = 1;
